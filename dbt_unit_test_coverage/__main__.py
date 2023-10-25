984b2877c2a28088915f633c782ed6167441532e
Bill <bill@thewallisfamily.co.uk>
2023-10-25 16:51:02 +0100

    🐛 fix: Add the correct import to the CLI

--- dbt_unit_test_coverage/__main__.py
+++ dbt_unit_test_coverage/__main__.py
@@ -1,8 +1,10 @@
 """
 Entry point for the ``dbt_unit_test_coverage`` package.
 """
+import typer
+
 import dbt_unit_test_coverage.main
 
-app = lambda: typer.run(dbt_unit_test_coverage.main.main)  # noqa
+app = lambda: typer.run(dbt_unit_test_coverage.main.main)  # noqa: E731
 if __name__ == "__main__":
     app()
